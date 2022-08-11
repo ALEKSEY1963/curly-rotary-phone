4b6f74f9d36dec2b300698e037b01f24fefb627d
ALEKSEY1963 <106527924+ALEKSEY1963@users.noreply.github.com>
2022-08-11 17:41:21 +0300

    Update dom_app.js

--- dom_app.js
+++ dom_app.js
@@ -1,22 +1,27 @@
-const slides = document.querySelectorAll(".slide");
 
+function slidesPlagin(activeSlide) {
+  const slides = document.querySelectorAll('.slide');
 
-slides.forEach((slide) => {
-  slide.addEventListener('click', () => {
-    clearActiveClasses()
-    slide.classList.add('active')
-  })
-});
-
-// for (const slide of slides) {
-//   slide.addEventListener("click", () => {
-//     clearActiveClasses();
-//     slide.classList.add("active");
-//   });
-// }
-
-function clearActiveClasses() {
+  slides[activeSlide].classList.add('active')
+  
   slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('active')
+    })
+  })
+  
+  // for (const slide of slides) {
+  //   slide.addEventListener("click", () => {
+  //     clearActiveClasses();
+  //     slide.classList.add("active");
+  //   });
+  // }
+  
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active")
+    })
+  }
 }
+slidesPlagin(0)
